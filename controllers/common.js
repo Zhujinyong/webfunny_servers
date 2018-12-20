e60b0a7b451139f595bfb4944974edb0745bd20a
jeffery <jiang1125712@163.com>
2018-12-20 14:09:45 +0800

    调整日志解析的方式

--- controllers/common.js
+++ controllers/common.js
@@ -42,15 +42,16 @@ class Common {
     const logArray = paramStr.split("$$$")
     for(var i = 0; i < logArray.length; i ++) {
       if (!logArray[i]) continue;
-      console.log(logArray[i])
       let logInfo = null;
       try {
         logInfo = JSON.parse("\"" + logArray[i] + "\"");
       } catch (error) {
         log.error(ctx, error, 0);
       }
-      console.log(logInfo)
       if (!logInfo) continue;
+      if (typeof logInfo === "string") {
+        logInfo = JSON.parse(logInfo)
+      }
       logInfo.monitorIp = clientIpString
       logInfo.province = province
       logInfo.city = city
