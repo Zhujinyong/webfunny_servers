27f60a07466fcac418d04b923d4196fd0b3866d6
jeffery <jiang1125712@163.com>
2018-12-24 14:13:08 +0800

    暂停线上的日志记录

--- controllers/common.js
+++ controllers/common.js
@@ -40,7 +40,7 @@ class Common {
     //   });
     // 暂时先把线上的日志记录过滤掉
     const paramStr = ctx.request.body.data
-    if (paramStr.indexOf("-qa") || paramStr.indexOf("-staging")) {
+    if (paramStr.indexOf("-qa") !== -1 || paramStr.indexOf("-staging") !== -1) {
       const param = JSON.parse(ctx.request.body.data)
       const logArray = param.logInfo.split("$$$")
       for(var i = 0; i < logArray.length; i ++) {
