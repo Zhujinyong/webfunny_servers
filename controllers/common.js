e6b8038a63e5a2be1a0aa08d25c675bf08ea7499
jeffery <jiang1125712@163.com>
2019-01-10 17:38:33 +0800

    日志处理

--- controllers/common.js
+++ controllers/common.js
@@ -40,7 +40,7 @@ class Common {
     //     }
     //   });
     // 暂时先把线上的日志记录过滤掉
-    const paramStr = ctx.request.body.data.replace(/\": Script error./g, "...")
+    const paramStr = ctx.request.body.data.replace(/": Script error\./g, "script error")
     // if (paramStr.indexOf("-qa") !== -1 || paramStr.indexOf("-staging") !== -1) {
     if (true) {
       const param = JSON.parse(ctx.request.body.data)
